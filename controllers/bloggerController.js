daa49a4dd5ad39a71375bcb73ec1113a8cca9aa8
chehin <chehinjuan@gmail.com>
2021-06-26 13:52:36 -0300

    GET a blogger

--- controllers/bloggerController.js
+++ controllers/bloggerController.js
@@ -1,3 +1,19 @@
+const axios = require('axios');
+
 exports.listar = () => {
-    console.log('Obtengo blogs de blogger');
+    // Make a request for a user with a given ID
+    axios.get('https://blogger.googleapis.com/v3/blogs/679124141950031513?maxPosts=1&view=VIEW_TYPE_UNSPECIFIED&key=AIzaSyBUwmENvyOHQgN3p7iYKeYFlXxiPkywjMI&')
+        .then(function(response) {
+            // handle success
+            console.log('response es : ', response);
+        })
+        .catch(function(error) {
+            // handle error
+            console.log('error es : ', error);
+        })
+        .then(function() {
+            // always executed
+            console.log('entra 2d0 then');
+        });
+
 }
