23f914b8a072c2fce58978cacd1e4e52a2a7b105
chehin <chehinjuan@gmail.com>
2021-07-10 20:48:56 -0300

    Renovacion token
    Se logro refrescar el token cuando devuelve error 401

--- controllers/spotifyController.js
+++ controllers/spotifyController.js
@@ -1,5 +1,10 @@
 var SpotifyWebApi = require('spotify-web-api-node');
 require('dotenv').config()
+const axios = require('axios')
+var qs = require('qs');
+
+var token = 'BQDqQiozEcUD5Zetyp-GH3qxBfW4veBqcm6Tp7WkUl2gIeOcRAr7bEup_Npdcf0uj7blZvUL9IzlaXlRcKI';
+
 
 // credentials are optional
 var spotifyApi = new SpotifyWebApi({
@@ -8,16 +13,29 @@ var spotifyApi = new SpotifyWebApi({
     redirectUri: 'http://localhost:3000'
 });
 
-spotifyApi.setAccessToken('BQDqQiozEcUD5Zetyp-GH3qxBfW4veBqcm6Tp7WkUl2gIeOcRAr7bEup_Npdcf0uj7blZvUL9IzlaXlRcKI');
+spotifyApi.setAccessToken(token);
 
 exports.listarNovedades = (req, res) => {
     spotifyApi.getNewReleases({ limit: 5, offset: 0, country: 'AR' })
         .then(function(data) {
+            console.log('data es 1 : ', data);
             return res.json(data.body);
-        }, function(err) {
+        }, async function(err) {
+            console.log("Something went wrong!", err.statusCode);
+
+            if (err.statusCode = 401) {
+                console.log("Entra codigo 401");
+
+                this.token = await dameTokenSpotify();
+
+                console.log("this.token : ", this.token);
+
+
+                // spotifyApi.setAccessToken(this.token);
+            }
             console.log("Something went wrong!", err);
         });
-
+    return;
 }
 
 exports.obtenerCanciones = (req, res) => {
@@ -28,4 +46,42 @@ exports.obtenerCanciones = (req, res) => {
         }, function(err) {
             console.error(err);
         });
+}
+
+async function dameTokenSpotify() {
+    console.log('entra dameTokenSpotify');
+
+    var data = qs.stringify({
+        'grant_type': 'client_credentials',
+        'client_id': process.env.CLIENT_ID_SPOTIFY,
+        'client_secret': process.env.CLIENT_SECRET_SPOTIFY
+    });
+    console.log('entra dameTokenSpotify 1');
+
+    var config = {
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cookie': '__Host-device_id=AQBda3oIJgC_zq5LgFqGh9f6oQdenputghP57Dl4HctT-4mmMPxAilBzICbbtr8rTlvu6R8ibnQQ1Av0ZryvUyJ6kpqXqKZHI_E'
+        },
+        data: data
+    };
+    console.log('entra dameTokenSpotify 2');
+
+    var respuestaAxios = await axios(config)
+        .then(function(response) {
+            console.log('response es : = ', JSON.stringify(response.data));
+            this.token = response.data.access_token;
+        })
+        .catch(function(error) {
+            console.log('error en axios es : ', error);
+        });
+    console.log('entra dameTokenSpotif 3 : ', respuestaAxios);
+
+    console.log('entra dameTokenSpotif 4 this.token : ', this.token);
+
+    spotifyApi.setAccessToken(this.token);
+    // Listar novedades
+
 }
